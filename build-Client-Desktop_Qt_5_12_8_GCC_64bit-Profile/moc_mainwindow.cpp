b9514fc907d981064397367d526fa151a31ead29
CrazyMrAll <1819980632@qq.com>
2023-08-28 17:38:42 +0800

    更新好友申请离线功能

---
+++ build-Client-Desktop_Qt_5_12_8_GCC_64bit-Profile/moc_mainwindow.cpp
@@ -0,0 +1,295 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Client/front_end/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.8. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[33];
+    char stringdata0[552];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 11), // "sendMessage"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 2), // "ID"
+QT_MOC_LITERAL(4, 27, 7), // "content"
+QT_MOC_LITERAL(5, 35, 20), // "sendAddFriendRequest"
+QT_MOC_LITERAL(6, 56, 21), // "sendRefreshFriendList"
+QT_MOC_LITERAL(7, 78, 13), // "gotoAddFriend"
+QT_MOC_LITERAL(8, 92, 21), // "gotoDealFriendRequest"
+QT_MOC_LITERAL(9, 114, 10), // "getMessage"
+QT_MOC_LITERAL(10, 125, 4), // "name"
+QT_MOC_LITERAL(11, 130, 9), // "isReceive"
+QT_MOC_LITERAL(12, 140, 11), // "getUserInfo"
+QT_MOC_LITERAL(13, 152, 3), // "ins"
+QT_MOC_LITERAL(14, 156, 5), // "email"
+QT_MOC_LITERAL(15, 162, 5), // "birth"
+QT_MOC_LITERAL(16, 168, 16), // "getFriendRequest"
+QT_MOC_LITERAL(17, 185, 14), // "addMessageItem"
+QT_MOC_LITERAL(18, 200, 13), // "addFriendItem"
+QT_MOC_LITERAL(19, 214, 17), // "changeMessageItem"
+QT_MOC_LITERAL(20, 232, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(21, 249, 7), // "current"
+QT_MOC_LITERAL(22, 257, 16), // "clearMessageItem"
+QT_MOC_LITERAL(23, 274, 15), // "clearFriendItem"
+QT_MOC_LITERAL(24, 290, 27), // "on_pushButton_input_clicked"
+QT_MOC_LITERAL(25, 318, 30), // "on_radioButton_message_toggled"
+QT_MOC_LITERAL(26, 349, 7), // "checked"
+QT_MOC_LITERAL(27, 357, 30), // "on_radioButton_friends_toggled"
+QT_MOC_LITERAL(28, 388, 31), // "on_radioButton_settings_toggled"
+QT_MOC_LITERAL(29, 420, 31), // "on_pushButton_addFriend_clicked"
+QT_MOC_LITERAL(30, 452, 35), // "on_pushButton_friendRequest_c..."
+QT_MOC_LITERAL(31, 488, 33), // "on_pushButton_addThisUser_cli..."
+QT_MOC_LITERAL(32, 522, 29) // "on_pushButton_refresh_clicked"
+
+    },
+    "MainWindow\0sendMessage\0\0ID\0content\0"
+    "sendAddFriendRequest\0sendRefreshFriendList\0"
+    "gotoAddFriend\0gotoDealFriendRequest\0"
+    "getMessage\0name\0isReceive\0getUserInfo\0"
+    "ins\0email\0birth\0getFriendRequest\0"
+    "addMessageItem\0addFriendItem\0"
+    "changeMessageItem\0QListWidgetItem*\0"
+    "current\0clearMessageItem\0clearFriendItem\0"
+    "on_pushButton_input_clicked\0"
+    "on_radioButton_message_toggled\0checked\0"
+    "on_radioButton_friends_toggled\0"
+    "on_radioButton_settings_toggled\0"
+    "on_pushButton_addFriend_clicked\0"
+    "on_pushButton_friendRequest_clicked\0"
+    "on_pushButton_addThisUser_clicked\0"
+    "on_pushButton_refresh_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       5,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    2,  119,    2, 0x06 /* Public */,
+       5,    1,  124,    2, 0x06 /* Public */,
+       6,    0,  127,    2, 0x06 /* Public */,
+       7,    0,  128,    2, 0x06 /* Public */,
+       8,    0,  129,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       9,    4,  130,    2, 0x0a /* Public */,
+      12,    5,  139,    2, 0x0a /* Public */,
+      16,    0,  150,    2, 0x0a /* Public */,
+      17,    2,  151,    2, 0x0a /* Public */,
+      18,    2,  156,    2, 0x0a /* Public */,
+      19,    1,  161,    2, 0x0a /* Public */,
+      22,    0,  164,    2, 0x0a /* Public */,
+      23,    0,  165,    2, 0x0a /* Public */,
+      24,    0,  166,    2, 0x08 /* Private */,
+      25,    1,  167,    2, 0x08 /* Private */,
+      27,    1,  170,    2, 0x08 /* Private */,
+      28,    1,  173,    2, 0x08 /* Private */,
+      29,    0,  176,    2, 0x08 /* Private */,
+      30,    0,  177,    2, 0x08 /* Private */,
+      31,    0,  178,    2, 0x08 /* Private */,
+      32,    0,  179,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,    4,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::Bool,    3,   10,    4,   11,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::QString,    3,   10,   13,   14,   15,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,   10,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    3,   10,
+    QMetaType::Void, 0x80000000 | 20,   21,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   26,
+    QMetaType::Void, QMetaType::Bool,   26,
+    QMetaType::Void, QMetaType::Bool,   26,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->sendMessage((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 1: _t->sendAddFriendRequest((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->sendRefreshFriendList(); break;
+        case 3: _t->gotoAddFriend(); break;
+        case 4: _t->gotoDealFriendRequest(); break;
+        case 5: _t->getMessage((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< bool(*)>(_a[4]))); break;
+        case 6: _t->getUserInfo((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< QString(*)>(_a[5]))); break;
+        case 7: _t->getFriendRequest(); break;
+        case 8: _t->addMessageItem((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 9: _t->addFriendItem((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 10: _t->changeMessageItem((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 11: _t->clearMessageItem(); break;
+        case 12: _t->clearFriendItem(); break;
+        case 13: _t->on_pushButton_input_clicked(); break;
+        case 14: _t->on_radioButton_message_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 15: _t->on_radioButton_friends_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 16: _t->on_radioButton_settings_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 17: _t->on_pushButton_addFriend_clicked(); break;
+        case 18: _t->on_pushButton_friendRequest_clicked(); break;
+        case 19: _t->on_pushButton_addThisUser_clicked(); break;
+        case 20: _t->on_pushButton_refresh_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWindow::*)(QString , QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::sendMessage)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::sendAddFriendRequest)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::sendRefreshFriendList)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::gotoAddFriend)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::gotoDealFriendRequest)) {
+                *result = 4;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QWidget::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 21;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::sendMessage(QString _t1, QString _t2)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void MainWindow::sendAddFriendRequest(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void MainWindow::sendRefreshFriendList()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void MainWindow::gotoAddFriend()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void MainWindow::gotoDealFriendRequest()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
